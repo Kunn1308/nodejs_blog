a2fa5303603f874f2ba685fec7cacd4641c96cfc
Minh Ngọc <ngockunn13801@gmail.com>
2023-12-31 12:44:50 +0700

    Refactor code: create routes, app/controller

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+
+function route(app){
+    // route
+    // app.get('/news', (req, res) => {
+    //     res.render('news');
+    // });
+    app.use('/news',newsRouter);
+    app.use('/',siteRouter);
+
+    
+
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.send('');
+    // });
+}
+
+module.exports = route;
