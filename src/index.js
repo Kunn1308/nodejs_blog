b53645296c96dfc649b3aa008c6bcba76f5bd34f
Minh Ngọc <ngockunn13801@gmail.com>
2024-04-26 12:55:48 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 const path = require('path');
-const express = require('express')
-const handlebars = require('express-handlebars')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const express = require('express');
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 //http logger
 // app.use(morgan('combined'))
 
 //template engine
-app.engine('hbs', handlebars.engine({extname:'.hbs'}));
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('');
-})
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
