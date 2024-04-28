8d909da8a48c46948fb17e732d01fcddd8d39649
Minh Ngọc <ngockunn13801@gmail.com>
2024-04-28 20:09:18 +0700

    [CRUD] Update course

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000;
 const route = require('./routes');
 const db = require('./config/db');
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 //connect to db
 db.connect();
@@ -17,7 +19,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(morgan('combined'))
 
 //template engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
