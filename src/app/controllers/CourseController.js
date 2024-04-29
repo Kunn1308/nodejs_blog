d095e8397106cde723e37c8aea22c7e46804cd54
Minh Ngọc <ngockunn13801@gmail.com>
2024-04-30 00:05:27 +0700

    làm checkbox all restored và remove vĩnh viễn bên trash/courses, fix thêm UI người dùng

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -72,9 +72,25 @@ class CourseController {
 
     //[POST] /courses/handle-form-actions
     handleFormActions(req, res, next) {
-        Course.delete({ _id: { $in: req.body.courseIds } })
-            .then(() => res.redirect('back'))
-            .catch(next);
+        switch (req.body.action) {
+            case 'patch':
+                Course.restore({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            case 'forceDelete':
+                Course.deleteMany({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is invalid' });
+        }
     }
 }
 
